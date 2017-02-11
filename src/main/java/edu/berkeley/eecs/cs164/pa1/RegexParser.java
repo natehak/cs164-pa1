4000c398cb845fa27972901183ca2d51533ffa51
Nathan Hakkakzadeh <nate.hak@gmail.com>
2017-02-11 15:11:32 -0800

    Scaffolding was stupid, got rid of it

--- src/main/java/edu/berkeley/eecs/cs164/pa1/RegexParser.java
+++ src/main/java/edu/berkeley/eecs/cs164/pa1/RegexParser.java
@@ -1,31 +1,5 @@
 package edu.berkeley.eecs.cs164.pa1;
 
-import java.util.LinkedList;
-
-/**
- * Simple tree structure from parser example.
- */
-class Node {
-    LinkedList children;
-    String type;
-
-    public Node(String type) {
-        this.type = type;
-        children = new LinkedList<>();
-    }
-
-    public void addChild(Object child) {
-        children.addLast(child);
-    }
-
-    @Override
-    public String toString() {
-        return type +
-                "(" + children +
-                ')';
-    }
-}
-
 /**
  * This class parses a simple regular expression syntax into an NFA
  */
@@ -42,11 +16,6 @@ public class RegexParser {
      */
     public static Automaton parse(String pattern) {
         // Why no tokenize step? Because each character is itself a token! (Disregarding escapes)
-        Node parseTree = createParseTree(pattern);
-        return null;
-    }
-
-    private static Node createParseTree(String input) {
         return null;
     }
 }
