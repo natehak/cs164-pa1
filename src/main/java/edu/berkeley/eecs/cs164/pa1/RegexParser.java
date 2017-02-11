eeb7f9b491ddca32ec61e003276b11087ea7a863
Nathan Hakkakzadeh <nate.hak@gmail.com>
2017-02-10 22:43:39 -0800

    Basic scaffolding

--- src/main/java/edu/berkeley/eecs/cs164/pa1/RegexParser.java
+++ src/main/java/edu/berkeley/eecs/cs164/pa1/RegexParser.java
@@ -1,5 +1,30 @@
 package edu.berkeley.eecs.cs164.pa1;
 
+import java.util.LinkedList;
+
+/**
+ * Simple tree structure from parser example.
+ */
+class Node {
+    LinkedList children;
+    String type;
+
+    public Node(String type) {
+        this.type = type;
+        children = new LinkedList<>();
+    }
+
+    public void addChild(Object child) {
+        children.addLast(child);
+    }
+
+    @Override
+    public String toString() {
+        return type +
+                "(" + children +
+                ')';
+    }
+}
 
 /**
  * This class parses a simple regular expression syntax into an NFA
@@ -16,7 +41,12 @@ public class RegexParser {
      * @throws RegexParseException upon encountering a parse error
      */
     public static Automaton parse(String pattern) {
+        // Why no tokenize step? Because each character is itself a token! (Disregarding escapes)
+        Node parseTree = createParseTree(pattern);
         return null;
     }
 
+    private static Node createParseTree(String input) {
+        return null;
+    }
 }
